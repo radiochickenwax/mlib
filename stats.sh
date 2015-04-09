2d56e72e89279f71bdd81df198a6e21f02fe472a
greg <gregorio.jp.mitchell@gmail.com>
2015-04-09 16:39:15 -0400

    Stripped it down a little.

--- stats.sh
+++ stats.sh
@@ -17,18 +17,19 @@ numargs=$#;
 
 
 
-if [ $numargs -lt 1 ]; then
-    printf "Usage: stats.sh {-rows|-cols} [input_file] \n";
+#if [ $numargs -lt 1 ]; then
+#    printf "Usage: stats.sh {-rows|-cols} [input_file] \n";
 
-else
-    #printf "\$1 = $1\n";
-    # check whether we're using rows or columns
-    if [ $1 != "-rows" ] && [ $1 != "-cols" ] ; then
-	printf "Usage: stats.sh {-rows|-cols} [input_file] \n";
+#else
+    if [ $1 = "-rows" ]; then
+	# sum rows
+	printf "Summing rows\n";
+    elif [ $1 = "-cols" ]; then
+	printf "Summing cols\n"
     else
-	printf "Good to go.\n";
+	printf "Usage: stats.sh {-rows|-cols} [input_file] \n";	    
     fi
 
     # check if a filename is given on the commandline
     
-fi
+#fi
