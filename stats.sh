469d3d8d6f787f2b6cf080064d481cc24fa2521e
greg <gregorio.jp.mitchell@gmail.com>
2015-04-10 10:51:12 -0400

    cleaned up a little

--- stats.sh
+++ stats.sh
@@ -32,45 +32,14 @@ if [ $# = 2 ]; then  # a filename is given.  Store it in a variable.
 
     # (browse-url "http://tldp.org/LDP/abs/html/fto.html")
     if [ -e $2 ]; then     # check that $filename exists 
-	printf "file exists\n";
-	printf "calculating average:\n"
-	
-	# read lines from file into an array
-
-	# (browse-url "http://stackoverflow.com/questions/11393817/bash-read-lines-in-file-into-an-array")
-
-	IFS=$'\n' GLOBIGNORE='*';  # what trickery is this?  
-	# read -d '' -r -a lines < $filename;  
-
-	# I prefer to use cat instead of read, it is more general.
-	lines=($(cat $filename));
-
-	# check number of lines
-	numLines=${#lines[@]};  # note the hashtag in lines.  Why?
-	
-	echo "numLines = $numLines";
-
-	# checkpoint:  test that lines are read properly.
-	# for (( i=0; i<$numLines ));  # I wish this syntax worked more easily
-	
-	# for line in $lines  # broken
-
-	# the following works.  It's unnecessary though.
-	#-----------
-	# for line in ${lines[@]}  # no hash tag here eh?  Why?!!
-	#  do
-	#     printf "%s\n" "$line";
-	#     #printf "%s\n" ${lines[$i]};
-	#  done;
-	
 
 	if [ $op = "rows" ]; then
 	    # read 
 	    rowAvg=$(expr 1 + 1);
 	    printf "rowAvg = $rowAvg\n";
 
-	else
-	    printf "no thatnks";
+	else # use cols
+	    printf "using cols\n";
 	fi
 	
     else # a filename is not given.
